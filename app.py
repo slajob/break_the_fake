919f16f2abfc61db7c80f681d96cb1d7717db83a
slawke <slawke@slawke>
2022-11-20 08:55:05 +0100

    scrap integrated

--- app.py
+++ app.py
@@ -15,17 +15,20 @@ def get_articles():
 def add_article():
     article_details = request.get_json()
     created_at = time.time()
+    url = article_details["url"]
     fake_rating = article_details["fake_rating"]
     fake_rating_community = article_details["fake_rating_community"]
     clickbait_rating = article_details["clickbait_rating"]
     clickbait_rating_community = article_details["clickbait_rating_community"]
-    published_at = article_details["published_at"]
-    title = article_details["title"]
-    lead = article_details["lead"]
+    # published_at = article_details["published_at"]
+    published_at = text(url)[0]
+    title = text(url)[1]
+    lead = text(url)[2]
     img_url = article_details["img_url"]
-    author = article_details["author"]
-    url = article_details["url"]
-    print(text(url))
+    author = text(url)[3]
+    #text returns: publication, title, lead, author
+    # text(url)[]
+
     result = sqdb.add_article(created_at, fake_rating, fake_rating_community, clickbait_rating,
                                             clickbait_rating_community, published_at, title, lead, img_url,
                                             author, url)
